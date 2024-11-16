9eefaecdd8654f29fc32acf03e337c2395e07047
LaurentBarres <91944674+Yoann-P@users.noreply.github.com>
2024-11-16 20:03:50 +0100

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
         </button>
       </nav>
       <h1 className="head_text">
-        Summarize Artciles with <br className="max-md:hidden" />
+        Summarize Articles with <br className="max-md:hidden" />
         <span className="orange_gradient"> OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
